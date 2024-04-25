521efea103b12b8b4b8312099611c75164a484e9
Antoine <mahassadijoseph@gmail.com>
2024-04-25 10:15:29 +0200

    nav-hero done

--- Js/gsap/loader.js
+++ Js/gsap/loader.js
@@ -13,6 +13,9 @@ function preLoadAnimHero() {
     tl.to('.text-2', { y: '0%', duration: 1, stragger: 1.5 });
     tl.to('.text-3', { y: '0%', duration: 1, stragger: 2 });
     tl.to('.img-hero', { y: '0%', duration: 2, stragger: 3 });
+    tl.to('.tag-position-2', { opacity: '1', duration: 1, stragger: 4 });
+    tl.to('.tag-position-1', { opacity: '1', duration: 1, stragger: 4 });
+    tl.to('.citation-position', { opacity: '1', duration: 1, stragger: 4 });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
